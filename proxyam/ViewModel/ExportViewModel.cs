0c923ccd6cf7d762182c3f30a3da4d38787ddda3
slavik <slavikok2@gmail.com>
2018-02-06 10:26:01 +0200

    init export

--- proxyam/ViewModel/ExportViewModel.cs
+++ proxyam/ViewModel/ExportViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
 using GalaSoft.MvvmLight;
@@ -8,20 +10,49 @@ using proxyam.Model;
 
 namespace proxyam.ViewModel
 {
-    public class ExportViewModel : ViewModelBase
+    public class KeyValue: ViewModelBase
+    {
+        private string _key;
+        private bool _value;
+
+        public KeyValue()
+        {
+          
+        }
+
+        public KeyValue(string key, bool value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public string Key
+        {
+            get { return _key; }
+            set => Set(() => Key, ref _key, value);
+        }
+
+        public bool Value
+        {
+            get { return _value; }
+            set => Set(() => Value, ref _value, value);
+        }
+    }
+
+    public class ExportViewModel : KeyValue
     {
         public MainViewModel MainViewModel { get; }
 
-        private IDictionary<string, bool> _proxyType;
+        private ObservableCollection<KeyValue> _proxyType;
 
         public ExportViewModel(MainViewModel main)
         {
             MainViewModel = main;
-            _proxyType = new Dictionary<string, bool>();
+            _proxyType = new ObservableCollection<KeyValue>();
             FillProxyType();
         }
 
-        public IDictionary<string, bool> ProxyType
+        public ObservableCollection<KeyValue> ProxyType
         {
             get => _proxyType;
             set => Set(() => ProxyType, ref _proxyType, value);
@@ -29,12 +60,12 @@ namespace proxyam.ViewModel
 
         private void FillProxyType()
         {
-            ProxyType.Add("proxies", false);
-            ProxyType.Add("ip", false);
-            ProxyType.Add("country", false);
-            ProxyType.Add("city", false);
-            ProxyType.Add("speed", false);
-            ProxyType.Add("uptime", false);
+            ProxyType.Add(new KeyValue("proxies", true));
+            ProxyType.Add(new KeyValue("ip", true));
+            ProxyType.Add(new KeyValue("country", true));
+            ProxyType.Add(new KeyValue("city", true));
+            ProxyType.Add(new KeyValue("speed", true));
+            ProxyType.Add(new KeyValue("uptime", true));
         }
     }
 }
